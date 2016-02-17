f74cfd3b29cc822dd29f5005d934e2e50ac1e8be
Jackson Burnette <jacksondburnette@gmail.com>
2016-02-16 17:56:41 -0800

    changes to hand.deal and new methods in Hand class

--- blackjack_classes.py
+++ blackjack_classes.py
@@ -43,12 +43,14 @@ class Player(object):
 
 class Card(object):
 
+    #A=1 by default
     int_values = {
-        '1':1, '2':2, '3':3,
+        '2':2, '3':3,
         '4':4, '5':5, '6':6,
         '7':7, '8':8, '9':9,
         '10':10, 'J':10, 'Q':10,
-        'K':10, 'low_A':1, 'high_A':11
+        'K':10, 'low_A':1, 'high_A':11,
+        'A':1
     }
 
     def __init__(self,suit,rank):
@@ -73,7 +75,7 @@ class Deck(object):
 
     def __init__(self,
                  suits=['H','C','D','S'],
-                 ranks=['1','2','3','4','5','6','7',
+                 ranks=['2','3','4','5','6','7',
                         '8','9','10','J','Q','K','A'],
                  contents=[]
                 ):
@@ -95,7 +97,44 @@ class Deck(object):
         """remove card object from deck contents"""
         self.contents.remove(card)
 
+    def top_card(self):
+        """return top card object in deck"""
+        return self.contents[0]
+
     def shuffle(self):
         """returns contents of deck, shuffled"""
         shuffle(self.contents)
-        return self.contents
+
+class Hand(object):
+
+    def __init__(self, contents=[]):
+        self.contents = contents
+
+    def add_card(self,card):
+        """add card object to hand contents"""
+        self.contents.append(card)
+
+    def sub_card(self,card):
+        """remove card object from deck contents"""
+        self.contents.remove(card)
+
+    def deal(self,deck,num_cards=2):
+         """make blackjack hand with given size (2 in blackjack) from deck object"""
+         i = 0
+         while i < num_cards:
+             pdb.set_trace()
+             #move card from deck to hand
+             self.add_card(deck.top_card())
+             deck.sub_card(deck.top_card())
+             i += 1
+
+    def show(self):
+        """show cards in hand"""
+        for card in self.contents:
+            print card.rank + ' of ' + card.suit
+
+    def value(self):
+        val = 0
+        for card in self.contents:
+            val += card.int_value()
+        return val
